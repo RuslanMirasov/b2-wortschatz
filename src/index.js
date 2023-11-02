21a875277704e736796b5b83c5de7d89439bff2a
RuslanMirasov <info@mirasov.dev>
2023-11-02 23:26:06 +0100

    start

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/b2-wortschatz">
       <App />
     </BrowserRouter>
   </React.StrictMode>
